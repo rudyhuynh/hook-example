a2aa8140b19a366d827f946ecedbf49be5846a6c
Rudy Huynh <rudy.huynh286@gmail.com>
2023-04-27 10:10:05 +0700

    update CounterApp with stale value issue

--- src/components/CounterApp.tsx
+++ src/components/CounterApp.tsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const CounterApp = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log(`[${new Date().toLocaleTimeString()}] Count: ${count}`);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const onClick = () => {
     setCount(count + 1);
